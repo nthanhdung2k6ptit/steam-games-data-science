a8064a327e72028e43df87462d626e0406427bbc
teanteagame <teanteagame@gmail.com>
2026-05-25 14:44:41 +0700

    last commit of me, perhaps.

--- src/models/classification.py
+++ src/models/classification.py
@@ -3,7 +3,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def train_classification_model(X_train, y_train, model_type = 'rfc'):
     if model_type == 'rfc':
-        model = RandomForestClassifier(n_estimators=100, random_state=67, class_weight='balanced')
+        model = RandomForestClassifier()
     else:
         model = KNeighborsClassifier()
     model.fit(X_train, y_train)
--- src/models/regression.py
+++ src/models/regression.py
@@ -1,11 +1,9 @@
-import joblib
-import os
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 
 def train_regression_model(X_train, y_train, model_type='rf'):   
     if model_type == 'rf':
-        model = RandomForestRegressor(n_estimators=100, random_state=67)
+        model = RandomForestRegressor()
     else:
         model = LinearRegression()
         
